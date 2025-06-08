0a25c05f56101f2e2ac9e6a0330f3e444aeb5b92
Mateusz <rzaiwruk@gmail.com>
2025-06-08 20:08:59 +0200

    a

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="flex w-screen items-center justify-center flex-row text-black">
-     <div className="flex h-screen items-center justify-center flex-col bg-white">
+    <div className="flex w-screen items-center justify-center flex-row text-black  bg-white">
+     <div className="flex h-screen items-center justify-center flex-col">
         <div className="bg-amber-500 p-7 rounded-2xl text-4xl drop-shadow-black">Mateusz Romanowski</div>
        <div>numer albumu: 124058</div>
       </div>
