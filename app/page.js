630d7edc60928ff49e66ac1fafc95058153ebb92
Mateusz <rzaiwruk@gmail.com>
2025-06-08 19:36:37 +0200

    a

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <>
     <div className="flex justify-center">
       <div className="bg-amber-500 p-4 rounded-4xl">Mateusz Romanowski</div>
+      <div>numer albumu: 124058</div>
     </div>
     </>
   );
