798774b06a359b3233234b7b72a99907453659d4
Mateusz <rzaiwruk@gmail.com>
2025-06-08 20:06:51 +0200

    a

--- app/page.js
+++ app/page.js
@@ -3,9 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="flex h-screen items-center justify-center flex-col bg-gradient-to-r bg-white">
-      <div className="bg-amber-500 p-7 rounded-2xl text-4xl drop-shadow-black">Mateusz Romanowski</div>
-      <div>numer albumu: 124058</div>
+    <div className="flex w-screen items-center justify-center flex-row">
+     <div className="flex h-screen items-center justify-center flex-col bg-white">
+        <div className="bg-amber-500 p-7 rounded-2xl text-4xl drop-shadow-black">Mateusz Romanowski</div>
+       <div>numer albumu: 124058</div>
+      </div>
+      <div>Moje top 5 serów</div>
     </div>
     </>
   );
