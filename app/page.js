b5a275738ceef4d0eb1de27e84b94d2bc4365214
Mateusz <rzaiwruk@gmail.com>
2025-06-08 19:04:01 +0200

    as

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>cos</div>
+    <>
+      <div className="bg-amber-400 p-5">Mateusz Romanowski</div>
+    </>
   );
 }
