214c399dccf44d2d3b04324be48058f541b87603
Mateusz <rzaiwruk@gmail.com>
2025-06-08 19:47:15 +0200

    a

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="flex justify-center columns-1">
+    <div className="flex justify-center flex-col">
       <div className="bg-amber-500 p-4 rounded-4xl">Mateusz Romanowski numer albumu: 124058</div>
       
     </div>
