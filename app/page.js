2148bd6e2139c1c72d11d1251dfa6199f7eeb4d3
Mateusz <rzaiwruk@gmail.com>
2025-06-22 00:32:31 +0200

    as

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
     <div className="flex items-center flex-row text-black  bg-white">
-     <div className="flex h-screen items-center justify-left flex-col">
+     <div className="flex h-screen items-center justify-center flex-col">
         <div className="p-7 rounded-2xl text-4xl drop-shadow-black">Mateusz Romanowski</div>
        <div>numer albumu: 124058</div>
       </div>
