34ed8ea0107dbdcc39e53a4cd0a1d3f4ce29dc02
Mateusz <rzaiwruk@gmail.com>
2025-06-22 00:31:39 +0200

    as

--- app/page.js
+++ app/page.js
@@ -8,9 +8,6 @@ export default function Home() {
         <div className="p-7 rounded-2xl text-4xl drop-shadow-black">Mateusz Romanowski</div>
        <div>numer albumu: 124058</div>
       </div>
-      <div className="flex h-20 w-800 items-start justify-start flex-col">
-      <div className="text-5xl font-extrabold">Moje top 5 serów</div>
-      </div>
     </div>
     </>
   );
